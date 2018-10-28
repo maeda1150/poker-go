3e7df2268d4a3faae5a81203f52e8779bfa0397f
maeda1150 <masaki19770108@gmail.com>
2018-10-28 20:02:10 +0900

    refactoring
    modified:   main.go

--- main.go
+++ main.go
@@ -30,77 +30,119 @@ func main() {
 		tryTimes = times
 	}
 
-	countFourOfAKind := 0
-	countFullHouse := 0
-	countFlush := 0
-	countStraight := 0
-	countThreeOfAKind := 0
-	countTwoPair := 0
-	countOnePair := 0
+	resultCount := NewResultCount()
 
 	bench := testing.Benchmark(func(b *testing.B) {
 
+		results := []Result{}
 		for t := 0; t < tryTimes; t++ {
-			deck := createDeck()
-			deck = removeCardsFromDeck(deck, hands)
-			shuffleDeck(deck)
-			boad := []Card{}
-			for i := 0; i < 5; i++ {
-				rand.Seed(time.Now().UnixNano())
-				boad = append(boad, deck[rand.Intn(50)])
-			}
-			all := append(hands, boad...)
+			result := playPreFlop(hands)
+			results = append(results, result)
 
-			isFourOfAKind := false
-			isFullHouse := false
-			isFlush := false
-			isStraight := false
-			isThreeOfAKind := false
-			isTwoPair := false
-			isOnePair := false
+		}
 
-			for com := range combinations(all, 5, 10) {
-				if fourOfAKind(com) {
-					isFourOfAKind = true
-				} else if fullHouse(com) {
-					isFullHouse = true
-				} else if flush(com) {
-					isFlush = true
-				} else if straight(com) {
-					isStraight = true
-				} else if threeOfAKind(com) {
-					isThreeOfAKind = true
-				} else if twoPair(com) {
-					isTwoPair = true
-				} else if onePair(com) {
-					isOnePair = true
-				}
-			}
-
-			if isFourOfAKind {
-				countFourOfAKind++
-			} else if isFullHouse {
-				countFullHouse++
-			} else if isFlush {
-				countFlush++
-			} else if isStraight {
-				countStraight++
-			} else if isThreeOfAKind {
-				countThreeOfAKind++
-			} else if isTwoPair {
-				countTwoPair++
-			} else if isOnePair {
-				countOnePair++
+		for _, result := range results {
+			if result.IsFourOfAKind {
+				resultCount.CountFourOfAKind++
+			} else if result.IsFullHouse {
+				resultCount.CountFullHouse++
+			} else if result.IsFlush {
+				resultCount.CountFlush++
+			} else if result.IsStraight {
+				resultCount.CountStraight++
+			} else if result.IsThreeOfAKind {
+				resultCount.CountThreeOfAKind++
+			} else if result.IsTwoPair {
+				resultCount.CountTwoPair++
+			} else if result.IsOnePair {
+				resultCount.CountOnePair++
 			}
 		}
 	})
 
-	fmt.Printf("OnePair      : %d ( %f percent )\n", countOnePair, float64(countOnePair)/float64(tryTimes)*100)
-	fmt.Printf("TwoPair      : %d ( %f percent )\n", countTwoPair, float64(countTwoPair)/float64(tryTimes)*100)
-	fmt.Printf("ThreeOfAKind : %d ( %f percent )\n", countThreeOfAKind, float64(countThreeOfAKind)/float64(tryTimes)*100)
-	fmt.Printf("Straight     : %d ( %f percent )\n", countStraight, float64(countStraight)/float64(tryTimes)*100)
-	fmt.Printf("Flush        : %d ( %f percent )\n", countFlush, float64(countFlush)/float64(tryTimes)*100)
-	fmt.Printf("FullHouse    : %d ( %f percent )\n", countFullHouse, float64(countFullHouse)/float64(tryTimes)*100)
-	fmt.Printf("FourOfAKind  : %d ( %f percent )\n", countFourOfAKind, float64(countFourOfAKind)/float64(tryTimes)*100)
+	fmt.Printf("OnePair      : %d ( %f percent )\n", resultCount.CountOnePair, float64(resultCount.CountOnePair)/float64(tryTimes)*100)
+	fmt.Printf("TwoPair      : %d ( %f percent )\n", resultCount.CountTwoPair, float64(resultCount.CountTwoPair)/float64(tryTimes)*100)
+	fmt.Printf("ThreeOfAKind : %d ( %f percent )\n", resultCount.CountThreeOfAKind, float64(resultCount.CountThreeOfAKind)/float64(tryTimes)*100)
+	fmt.Printf("Straight     : %d ( %f percent )\n", resultCount.CountStraight, float64(resultCount.CountStraight)/float64(tryTimes)*100)
+	fmt.Printf("Flush        : %d ( %f percent )\n", resultCount.CountFlush, float64(resultCount.CountFlush)/float64(tryTimes)*100)
+	fmt.Printf("FullHouse    : %d ( %f percent )\n", resultCount.CountFullHouse, float64(resultCount.CountFullHouse)/float64(tryTimes)*100)
+	fmt.Printf("FourOfAKind  : %d ( %f percent )\n", resultCount.CountFourOfAKind, float64(resultCount.CountFourOfAKind)/float64(tryTimes)*100)
 	fmt.Println(strconv.FormatFloat(bench.T.Seconds(), 'f', 20, 64) + " sec")
+}
+
+func playPreFlop(hands []Card) Result {
+	result := NewResult()
+
+	deck := createDeck()
+	deck = removeCardsFromDeck(deck, hands)
+	shuffleDeck(deck)
+	boad := []Card{}
+	for i := 0; i < 5; i++ {
+		rand.Seed(time.Now().UnixNano())
+		boad = append(boad, deck[rand.Intn(50)])
+	}
+	all := append(hands, boad...)
+
+	for com := range combinations(all, 5, 10) {
+		if fourOfAKind(com) {
+			result.IsFourOfAKind = true
+		} else if fullHouse(com) {
+			result.IsFullHouse = true
+		} else if flush(com) {
+			result.IsFlush = true
+		} else if straight(com) {
+			result.IsStraight = true
+		} else if threeOfAKind(com) {
+			result.IsThreeOfAKind = true
+		} else if twoPair(com) {
+			result.IsTwoPair = true
+		} else if onePair(com) {
+			result.IsOnePair = true
+		}
+	}
+	return result
+}
+
+type Result struct {
+	IsFourOfAKind  bool
+	IsFullHouse    bool
+	IsFlush        bool
+	IsStraight     bool
+	IsThreeOfAKind bool
+	IsTwoPair      bool
+	IsOnePair      bool
+}
+
+func NewResult() Result {
+	result := Result{}
+	result.IsFourOfAKind = false
+	result.IsFullHouse = false
+	result.IsFlush = false
+	result.IsStraight = false
+	result.IsThreeOfAKind = false
+	result.IsTwoPair = false
+	result.IsOnePair = false
+	return result
+}
+
+type ResultCount struct {
+	CountFourOfAKind  int
+	CountFullHouse    int
+	CountFlush        int
+	CountStraight     int
+	CountThreeOfAKind int
+	CountTwoPair      int
+	CountOnePair      int
+}
+
+func NewResultCount() ResultCount {
+	resultCount := ResultCount{}
+	resultCount.CountFourOfAKind = 0
+	resultCount.CountFullHouse = 0
+	resultCount.CountFlush = 0
+	resultCount.CountStraight = 0
+	resultCount.CountThreeOfAKind = 0
+	resultCount.CountTwoPair = 0
+	resultCount.CountOnePair = 0
+	return resultCount
 }
