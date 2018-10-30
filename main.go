29585279f0622214f1ceeda810fb95de143f1516
maeda1150 <masaki19770108@gmail.com>
2018-10-30 22:43:02 +0900

    fix measurement time
    modified:   main.go

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 	"os"
 	"strconv"
 	"sync"
-	"testing"
+	"time"
 )
 
 func main() {
+	start := time.Now()
+
 	suits, nums := splitSuitsAndNumbers(os.Args[1])
 
 	if (len(suits) != 2) || (len(nums) != 2) {
@@ -31,31 +33,28 @@ func main() {
 
 	resultCount := NewResultCount()
 
-	bench := testing.Benchmark(func(b *testing.B) {
+	wg := new(sync.WaitGroup)
+	results := make(chan []Result, 10)
+	for t := 0; t < 10; t++ {
+		wg.Add(1)
+		go func(hands []Card) {
+			defer wg.Done()
+			playPreFlopWithTimes(hands, tryTimes/10, results)
+		}(hands)
+	}
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
 
-		wg := new(sync.WaitGroup)
-		results := make(chan []Result, 10)
-		for t := 0; t < 10; t++ {
-			wg.Add(1)
-			go func(hands []Card) {
-				defer wg.Done()
-				playPreFlopWithTimes(hands, tryTimes/10, results)
-			}(hands)
+	rs := []Result{}
+	for result := range results {
+		for _, r := range result {
+			rs = append(rs, r)
 		}
-		go func() {
-			wg.Wait()
-			close(results)
-		}()
+	}
 
-		rs := []Result{}
-		for result := range results {
-			for _, r := range result {
-				rs = append(rs, r)
-			}
-		}
-
-		resultCount = calcResultCount(rs)
-	})
+	resultCount = calcResultCount(rs)
 
 	fmt.Printf("OnePair        : %d ( %f %v )\n", resultCount.CountOnePair, float64(resultCount.CountOnePair)/float64(tryTimes)*100, "%")
 	fmt.Printf("TwoPair        : %d ( %f %v )\n", resultCount.CountTwoPair, float64(resultCount.CountTwoPair)/float64(tryTimes)*100, "%")
@@ -65,5 +64,7 @@ func main() {
 	fmt.Printf("FullHouse      : %d ( %f %v )\n", resultCount.CountFullHouse, float64(resultCount.CountFullHouse)/float64(tryTimes)*100, "%")
 	fmt.Printf("FourOfAKind    : %d ( %f %v )\n", resultCount.CountFourOfAKind, float64(resultCount.CountFourOfAKind)/float64(tryTimes)*100, "%")
 	fmt.Printf("StraightFlush  : %d ( %f %v )\n", resultCount.CountStraightFlush, float64(resultCount.CountStraightFlush)/float64(tryTimes)*100, "%")
-	fmt.Println(strconv.FormatFloat(bench.T.Seconds(), 'f', 20, 64) + " sec")
+
+	elapsed := time.Since(start)
+	fmt.Println(elapsed)
 }
