125bac29679f0d5709a2d25026b1bcb4e20ac946
maeda1150 <masaki19770108@gmail.com>
2018-11-10 17:06:25 +0900

    add main.sh

---
+++ main.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+echo '## Pre-Flop'
+./poker-go s1h2
+echo
+./poker-go s1h3
+echo
+./poker-go s1h4
+echo
+./poker-go s1h8
+echo
+./poker-go s1h13
+echo
+./poker-go s3h4
+echo
+./poker-go s3h5
+echo
+./poker-go s3h6
+echo
+./poker-go s3h7
+echo
+./poker-go s3h8
+echo
+./poker-go s3h9
+echo
+./poker-go s7h8
+echo
+./poker-go s7h9
+echo
+./poker-go s7h10
+echo
+./poker-go s7h11
+echo
+./poker-go s12h13
+echo
+./poker-go s1s2
+echo
+./poker-go s1s13
+echo
+./poker-go s12s13
+echo
+./poker-go s9s10
+echo
+./poker-go s1h1
+echo
+./poker-go s8h8
+echo
+./poker-go s13h13
